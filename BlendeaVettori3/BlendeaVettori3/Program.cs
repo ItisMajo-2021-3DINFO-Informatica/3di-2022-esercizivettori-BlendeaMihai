a3520a2bf48141be7bf059f395137baabf3b7500
BlendeaMihai <98150787+BlendeaMihai@users.noreply.github.com>
2022-03-11 10:04:38 +0100

    Vettori3

---
+++ BlendeaVettori3/BlendeaVettori3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BlendeaVettori3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         const  int dimensione = 30;
+            int[] numero = new int[dimensione];
+            Random gen = new Random();
+            int somma = 0;
+            int media = 0;
+            for (int c = 0; c < dimensione; c++)
+            {
+                int num=gen.Next(0, 50);
+                numero[c] = num;
+               somma = somma + numero[c];
+                media = somma / dimensione;
+               
+            }
+            Console.Write($"La media dei 30 valori inseriti è {media}");
+
+        }
+    }
+}
