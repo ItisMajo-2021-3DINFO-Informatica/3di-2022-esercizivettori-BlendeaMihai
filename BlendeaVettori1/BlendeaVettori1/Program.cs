e5e982100b2c5205af354408fa303d3401ad5b71
BlendeaMihai <98150787+BlendeaMihai@users.noreply.github.com>
2022-03-11 09:40:46 +0100

    Vettori1

---
+++ BlendeaVettori1/BlendeaVettori1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BlendeaVettori1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int dimensione = 400;
+            int[] numero = new int[dimensione];
+            Random gen = new Random();
+
+            for(int c=0; c<dimensione; c++)
+            {
+                int num = gen.Next(1, 100);
+                numero[c] = num;
+                Console.WriteLine($"num{c}="+numero[c]);
+            }
+            
+
+        }
+    }
+}
