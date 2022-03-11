58bb721dd34a04425f1c6c975669383d1ef40a80
BlendeaMihai <98150787+BlendeaMihai@users.noreply.github.com>
2022-03-11 09:55:09 +0100

    Vettori2

---
+++ BlendeaVettori2/BlendeaVettori2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BlendeaVettori2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int dimensione = 400;
+            int[] numero = new int[dimensione];
+            Random Gen = new Random();
+            int maggiori = 0;
+            for(int c=0;c<dimensione; c++)
+            {
+                int num = Gen.Next(1, 100);
+                numero[c] = num;
+            
+            if( num > 50)
+                {
+
+                    maggiori = num;
+
+                }
+            }
+            Console.Write($"I numeri maggiori di 50 sono= {maggiori}");
+        }
+    }
+}
