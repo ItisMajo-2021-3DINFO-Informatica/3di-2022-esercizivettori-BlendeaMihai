d3e8719dba8c8c7e5d58dcbb1dc7df22ef14feb6
BlendeaMihai <98150787+BlendeaMihai@users.noreply.github.com>
2022-03-25 10:38:20 +0100

    Esercizio6

--- BlendeaEsercizio6/BlendeaEsercizio6/Program.cs
+++ BlendeaEsercizio6/BlendeaEsercizio6/Program.cs
@@ -13,7 +13,7 @@ namespace BlendeaEsercizio6
             int scarti = 0;
             for(int c = 0; c < dimensione; c++)
             {
-                int num = Gen.Next(20, 120);
+                int num = Gen.Next(20, 60);
                 numero[c] = num;
 
                 if (minore > numero[c])
@@ -24,13 +24,21 @@ namespace BlendeaEsercizio6
             }
             for (int c = 0; c < dimensione; c++)
             {
-                Console.WriteLine($"I Tempi sono: {numero[c]}");
+                Console.WriteLine($"I Tempi sono: {numero[c]} secondi");
                 
             }
 
 
             Console.WriteLine($"Il tempo più piccolo è:{minore}");
-            Console.WriteLine($"Gli scarti tra il tempo piu veloce e gli altri sono di({numero[0]},{numero[1]},{numero[2]},{numero[3]},{numero[4]},{numero[5]},{numero[6]},{numero[7]})");
+            Console.WriteLine($"Lo scarto del giocatore più veloce con il primo giocatore e di {numero[0] - minore} secondi");
+            Console.WriteLine($"Lo scarto del giocatore più veloce con il secondo giocatore è di {numero[1] - minore} secondi");
+            Console.WriteLine($"Lo scarto del giocatore più veloce con il terzo giocatore è di {numero[2] - minore} secondi");
+            Console.WriteLine($"Lo scarto del giocatore più veloce con il quarto giocatore è di {numero[3] - minore} secondi");
+            Console.WriteLine($"Lo scarto del giocatore più veloce con il quinto giocatore è di {numero[4] - minore} secondi");
+            Console.WriteLine($"Lo scarto del giocatore più veloce con il sesto giocatore è di {numero[5] - minore} secondi");
+            Console.WriteLine($"Lo scarto del giocatore più veloce con il settimo giocatore è di {numero[6] - minore} secondi");
+            Console.WriteLine($"Lo scarto del giocatore più veloce con il settimo giocatore è di {numero[7] - minore} secondi");
         }
     }
 }
+
