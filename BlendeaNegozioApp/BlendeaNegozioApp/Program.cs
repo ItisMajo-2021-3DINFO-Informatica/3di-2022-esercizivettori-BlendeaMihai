e9164ecf20434286961780c661b30063ed4d78e6
BlendeaMihai <98150787+BlendeaMihai@users.noreply.github.com>
2022-03-25 10:22:35 +0100

    EsercizioNegozioApp

---
+++ BlendeaNegozioApp/BlendeaNegozioApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BlendeaNegozioApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int dimensione = 2;
+            decimal[] prodotti = new decimal[dimensione];
+            decimal prezzoscontato = 0;
+            for (int c = 0; c < dimensione; c++)
+            {
+                Console.WriteLine($"Il prezzo del {c} prodotto");
+                prodotti[c] = Convert.ToDecimal(Console.ReadLine());
+            }
+            for (int c = 0; c < dimensione; c++)
+            {
+            prezzoscontato = prodotti[c]/100 * 78;
+            Console.WriteLine($"Il prezzo scontato del {c} prodotto è {prezzoscontato}");
+            }
+            
+        }
+    }
+}
